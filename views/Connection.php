72658eeb49b43a87d122e8823b900e3a02bf98ff
Teaflex <teaflex.dev@hotmail.com>
2021-10-14 20:26:58 +0200

    better for dom ?

--- views/Connection.php
+++ views/Connection.php
@@ -1,6 +1,6 @@
 <form action="POST">
     <label for="email">Email:</label>
-    <input type="email" name="email" id="email">
+    <input type="email" name="email" id="email"/>
     <label for="password">Password:</label>
-    <input type="password" name="password" id="password">
+    <input type="password" name="password" id="password"/>
 </form>
